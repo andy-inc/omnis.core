59aeea59ffac4bcb42595014a7b998e26ebaf315
Andy <andy@sumskoy.com>
2014-03-04 22:56:36 +0400

    add middleware plugin support

--- examples/server/plugins/rewrite.js
+++ examples/server/plugins/rewrite.js
@@ -1,3 +1,17 @@
 /**
  * Created by Andy <andy@sumskoy.com> on 04/03/14.
  */
+var plugin = function(){
+    return function(req, res){
+        if  (req.url == '/'){
+            req.url = '/api/index';
+        }
+    };
+};
+
+module.exports = exports = {
+    name: "rewrite",
+    type: "middleware:before:all",
+    filename: __filename,
+    fn: plugin
+};
